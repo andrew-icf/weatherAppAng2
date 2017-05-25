7dd31e74d4b7b2445da1acee96c83c3bf2cfec81
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2017-05-25 16:13:54 -0600

    building out header and other two pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentComponent } from './current/current.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CurrentComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
